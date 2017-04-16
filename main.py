eb86409e2935d57a8d9f7d47ed532db4bd0b7979
Hale Knox <halelovestrains11@gmail.com>
2017-04-15 22:02:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,18 @@ import RPi.GPIO as GPIO
 import api
 import rvm
 
-def ibutton_read():
-    base_dir = "/sys/devices/w1_bus_master1/w1_master_slaves"
-    delete_dir = "/sys/devices/w1_bus_master1/w1_master_remove"
-    while True:
-        in_data = open(base_dir, "r")
-        ib = in_data.read()
-        ib_st = ib.strip()
-        in_data.close()
-        del = open(delete_dir, "w")
-        del.write(ib_st)
-        del.flush()
-        return ib_st
+def read_rfid():
+    id = ""
+    read = rfid.read()
+    if read == "\x02":
+        for i in range(12):
+            read = rfid.read()
+            id = id + str(read)
+            serial.reset_input_buffer()
+        return id
 
 def insert_mode():
-    ib = ibutton_read()
+    ib = read_rfid()
     uid = api.ibutton2uid(ib)
     ins = rvm.start()
     cr = ins * 5
