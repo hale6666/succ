a07c979b90124e5357c71fcc89a9d492c76a3c6d
Hale Knox <knoxhale@gmail.com>
2017-04-19 13:53:24 -0400

    some fixes

--- rvm.py
+++ rvm.py
@@ -3,6 +3,13 @@ from time import sleep
 import requests
 import json
 import api
+import signal
+
+class AlarmException(Exception):
+    pass
+
+def alarm_handler(signum, frame):
+    raise AlarmException
  
 def forward(x):
     GPIO.output(13, GPIO.HIGH)
@@ -30,6 +37,13 @@ def make_list():
     st = {line.strip() for line in fl}
     fl.close()
 
+def inpu():
+    signal.signal(signal.SIGALRM, alarm_handler)
+    signal.alarm(10)
+    try:
+        return input("Insert Container\n")
+    except AlarmException:
+        return "0"
 
 def start():
     GPIO.setwarnings(False)
@@ -39,7 +53,9 @@ def start():
     inp = ""
     inserted = 0
     while inp != "0":
-        inp = input("Insert Container\n")
+        signal.alarm(10)
+        inp = inpu()
+        signal.alarm(0)
         if inp == "0": break
         inserted += check(inp)
     return inserted
