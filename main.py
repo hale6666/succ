73b01cf169e0e3637c480739181716ddb88a8c81
Hale Knox <halelovestrains11@gmail.com>
2017-04-19 16:31:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ import serial
 from time import sleep
 import rvm
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
-GPIO.setup(16, GPIO.OUT) #RFID Port
-GPIO.setup(18, GPIO.OUT) #RFID Port
-GPIO.output(16,False)
-GPIO.output(18,False)
-rfid = serial.Serial('/dev/ttyAMA0', 9600)
+os.system('modprobe wire timeout=1 slave_ttl=5')
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-smem')
+os.system('chmod a+w /sys/devices/w1_bus_master1/w1_master_slaves')
+os.system('chmod a+w /sys/devices/w1_bus_master1/w1_master_remove')
+os.system('chmod a+w /sys/devices/w1_bus_master1/w1_master_search')
+base_dir = '/sys/devices/w1_bus_master1/w1_master_slaves'
+delete_dir = '/sys/devices/w1_bus_master1/w1_master_remove'
 
+"""
 def read_rfid():
     id = ""
     print("Ready for RFID tag.")
@@ -23,9 +25,19 @@ def read_rfid():
             read = rfid.read()
             id = id + str(read)[2]
         return id
+"""
+
+def read_ib():
+    f = open(base_dir, "r")
+    ID = f.read()
+    f.close()
+    if ID != 'not found.\n':
+        d = open(delete_dir, "w")
+        d.write(ID)
+        return ID
 
 def insert_mode():
-    ib = read_rfid()
+    ib = read_ib()
     uid = api.ibutton2uid(ib)
     credits = api.get_credits(uid)
     print(uid, " : ", credits, " credits")
