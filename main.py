2c7694d37aaf24812123ef8b1e99296a53f8060e
Hale Knox <knoxhale@gmail.com>
2017-04-20 03:43:16 +0000

    security fixes

--- main.py
+++ main.py
@@ -5,12 +5,6 @@ import serial
 from time import sleep
 import rvm
 
-os.system('modprobe wire timeout=1 slave_ttl=5')
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-smem')
-os.system('chmod a+w /sys/devices/w1_bus_master1/w1_master_slaves')
-os.system('chmod a+w /sys/devices/w1_bus_master1/w1_master_remove')
-os.system('chmod a+w /sys/devices/w1_bus_master1/w1_master_search')
 base_dir = '/sys/devices/w1_bus_master1/w1_master_slaves'
 delete_dir = '/sys/devices/w1_bus_master1/w1_master_remove'
 
